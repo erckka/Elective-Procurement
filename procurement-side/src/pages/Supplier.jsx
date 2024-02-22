25d2db8233f98f13f05f9acd25cad26c064857cd
sushiraw <angeloayusongacilan@gmail.com>
2024-02-22 21:01:52 +0800

    Modal
    added modal for supplier

--- procurement-side/src/pages/Supplier.jsx
+++ procurement-side/src/pages/Supplier.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 // import NavBarMobile from '../components/Nav/NavBarMobile'
 import NavBar from '../components/Nav/NavBar'
 import Heading from '../components/Header/supplierheading'
+import AddModal from '../components/Modals/AddSupplier'
 
 const Supplier = () => {
   return (
     <div>
       <NavBar />
+      <AddModal />
       <Heading />
       {/* <NavBarMobile /> */}
     </div>
