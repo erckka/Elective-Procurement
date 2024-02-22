dfd7b271dd651fedcef6eec119469da5f5e22b3f
erckka <130682255+erckka@users.noreply.github.com>
2024-02-22 15:39:43 +0800

    added heading

---
+++ procurement-side/src/components/Header/supplierheading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const supplierheading = () => {
+  return (
+    <div className="flex justify-end mt-10">
+      <div className="bg-red-400 w-72">Supplier</div>
+    </div>
+  )
+}
+
+export default supplierheading
--- procurement-side/src/pages/Supplier.jsx
+++ procurement-side/src/pages/Supplier.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 // import NavBarMobile from '../components/Nav/NavBarMobile'
 import NavBar from '../components/Nav/NavBar'
+import Heading from '../components/Header/supplierheading'
 
 const Supplier = () => {
   return (
     <div>
       <NavBar />
+      <Heading />
       {/* <NavBarMobile /> */}
-      {/* Add your supplier-specific content here */}
     </div>
   )
 }
